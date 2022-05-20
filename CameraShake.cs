2fc10c87df76e9d59f20c2468b3f340e37d1b1fd
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-05-20 17:51:57 -0500

    Cleanup

--- CameraShake.cs
+++ CameraShake.cs
@@ -1,117 +1,78 @@
-using Godot;
-using System;
+global using Godot;
+global using System;
 
-public class CameraShake : Camera2D
+namespace GodotModules
 {
-    private GTimer _timerDuration;
-    private GTimer _timerFrequency;
-    private float _amplitudeX = 5;
-    private float _amplitudeY = 5;
-    private int _duration = 500;
-    private int _frequency = 50;
-
-    public override void _Ready()
+    public class CameraShake : Camera2D
     {
-        _timerDuration = new GTimer(this, nameof(StopProcess), _duration, false, false);
-        _timerFrequency = new GTimer(this, nameof(Shake), _frequency, true, false);
-        SetPhysicsProcess(false);
-    }
+        private GTimer _timerDuration;
+        private GTimer _timerFrequency;
+        private float _amplitudeX = 7;
+        private float _amplitudeY = 5;
+        private int _duration = 200;
+        private int _frequency = 40;
 
-    private void _on_Amplitude_X_text_changed(string text)
-    {
-        if (int.TryParse(text, out int result))
-            _amplitudeX = result;
-    }
-
-    private void _on_Amplitude_Y_text_changed(string text)
-    {
-        if (int.TryParse(text, out int result))
-            _amplitudeY = result;
-    }
-
-    private void _on_Duration_text_changed(string text)
-    {
-        if (int.TryParse(text, out int result))
-            _duration = result;
-    }
-
-    private void _on_Frequency_text_changed(string text)
-    {
-        if (int.TryParse(text, out int result))
-            _frequency = result;
-    }
-
-    private void Shake()
-    {
-        var randomX = GD.Randf() - 0.5f;
-        var randomY = GD.Randf() - 0.5f;
-
-        Offset = new Vector2(randomX * _amplitudeX, randomY * _amplitudeY);
-    }
-
-    public override void _PhysicsProcess(float delta)
-    {
-        
-    }
-
-    public override void _Input(InputEvent @event)
-    {
-        if (Input.IsActionJustPressed("ui_accept")) 
+        public override void _Ready()
         {
+            _timerDuration = new GTimer(this, nameof(StopShake), _duration, false, false);
+            _timerFrequency = new GTimer(this, nameof(SimulateShake), _frequency, true, false);
+        }
+
+        public override void _Input(InputEvent @event)
+        {
+            if (Input.IsActionJustPressed("ui_accept"))
+            {
+                StartShake(_amplitudeX, _amplitudeY, _duration, _frequency);
+            }
+        }
+
+        private void _on_Amplitude_X_text_changed(string text)
+        {
+            if (int.TryParse(text, out int result))
+                _amplitudeX = result;
+        }
+
+        private void _on_Amplitude_Y_text_changed(string text)
+        {
+            if (int.TryParse(text, out int result))
+                _amplitudeY = result;
+        }
+
+        private void _on_Duration_text_changed(string text)
+        {
+            if (int.TryParse(text, out int result))
+                _duration = result;
+        }
+
+        private void _on_Frequency_text_changed(string text)
+        {
+            if (int.TryParse(text, out int result))
+                _frequency = result;
+        }
+
+        private void SimulateShake()
+        {
+            var randomX = GD.Randf() - 0.5f;
+            var randomY = GD.Randf() - 0.5f;
+
+            Offset = new Vector2(randomX * _amplitudeX, randomY * _amplitudeY);
+        }
+
+        private void StartShake(float amplitudeX = 7, float amplitudeY = 5, int duration = 200, int frequency = 40)
+        {
+            _amplitudeX = amplitudeX;
+            _amplitudeY = amplitudeY;
+            _duration = duration;
+            _frequency = frequency;
+
             _timerDuration.StartMs(_duration);
             _timerFrequency.StartMs(_frequency);
-            SetPhysicsProcess(true);
+        }
+
+        private void StopShake()
+        {
+            _timerFrequency.Stop();
+            Offset = Vector2.Zero;
         }
     }
-
-    private void StopProcess()
-    {
-        SetPhysicsProcess(false);
-        _timerFrequency.Stop();
-        Offset = Vector2.Zero;
-    }
-}
-
-public class GTimer
-{
-    private readonly Timer _timer = new Timer();
-
-    public float Delay { get; set; }
-
-    public GTimer(Node target, int delayMs = 1000, bool loop = false, bool autoStart = false) =>
-        Init(target, delayMs, loop, autoStart);
-
-    public GTimer(Node target, string methodName, int delayMs = 1000, bool loop = true, bool autoStart = true)
-    {
-        Init(target, delayMs, loop, autoStart);
-        _timer.Connect("timeout", target, methodName);
-    }
-
-    private void Init(Node target, int delayMs, bool loop, bool autoStart)
-    {
-        _timer.WaitTime = delayMs / 1000f;
-        Delay = _timer.WaitTime;
-        _timer.OneShot = !loop;
-        _timer.Autostart = autoStart;
-        target.AddChild(_timer);
-    }
-
-    public bool IsActive() => _timer.TimeLeft != 0;
-    public void SetDelay(float delay) => _timer.WaitTime = delay;
-    public void SetDelayMs(int delayMs) => _timer.WaitTime = delayMs / 1000f;
-
-    public void Start(float delay)
-    {
-        _timer.WaitTime = delay;
-        Start();
-    }
-    public void StartMs(float delayMs)
-    {
-        _timer.WaitTime = delayMs / 1000;
-        Start();
-    }
-
-    public void Start() => _timer.Start();
-    public void Stop() => _timer.Stop();
-    public void QueueFree() => _timer.QueueFree();
 }
