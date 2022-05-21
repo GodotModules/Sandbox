fd2ab86af7c66b4963429b2479c204579346f195
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-05-20 22:30:37 -0500

    update

--- Scenes/Hotkeys/UIHotkeys.cs
+++ Scenes/Hotkeys/UIHotkeys.cs
@@ -4,24 +4,27 @@ namespace GodotModules
     {
         [Export] protected readonly NodePath NodePathHotkeyList;
         private Control _hotkeyList;
-        private Dictionary<string, List<InputEventInfo>> _inputEvents;
+        private Dictionary<string, HotkeyInfo> _defaultInputEvents;
 
         public override void _Ready()
         {
-            _inputEvents = new Dictionary<string, List<InputEventInfo>>();
+            _defaultInputEvents = new Dictionary<string, HotkeyInfo>();
             _hotkeyList = GetNode<Control>(NodePathHotkeyList);
-            
+
             // make sure all lists are defined
-            foreach (string actionGroup in InputMap.GetActions()) 
+            foreach (string actionGroup in InputMap.GetActions())
             {
                 var actions = InputMap.GetActionList(actionGroup);
 
                 if (actions.Count == 0)
                     continue;
 
-                _inputEvents[actionGroup] = new List<InputEventInfo>();
+                _defaultInputEvents[actionGroup] = new HotkeyInfo {
+                    Category = "ui",
+                    InputEventInfo = new List<InputEventInfo>()
+                };
             }
-            
+
             LoadDefaults();
         }
 
@@ -38,73 +41,91 @@ namespace GodotModules
                 _hotkeyList.AddChild(uiHotkey);
                 uiHotkey.SetLabel(actionGroup);
 
+                var category = GetCategory(actionGroup);
+
                 foreach (var action in actions)
                 {
                     if (action is InputEvent inputEvent)
                     {
-                        if (inputEvent is InputEventKey k)
+                        switch (inputEvent)
                         {
-                            _inputEvents[actionGroup].Add(
-                                new InputEventKeyInfo {
-                                    InputEventInfoType = InputEventInfoType.Key,
-                                    Alt = k.Alt,
-                                    Command = k.Command,
-                                    Control = k.Control,
-                                    Device = k.Device,
-                                    Echo = k.Echo,
-                                    Meta = k.Meta,
-                                    PhysicalScancode = k.PhysicalScancode,
-                                    Pressed = k.Pressed,
-                                    Scancode = k.Scancode,
-                                    Shift = k.Shift,
-                                    Unicode = k.Unicode
-                                }
-                            );
+                            case InputEventKey k:
+                                var e = _defaultInputEvents[actionGroup];
+                                e.Category = category;
+                                e.InputEventInfo.Add(ConvertToKeyInfo(k));
+                                break;
+                            case InputEventMouseButton m:
+                                _defaultInputEvents[actionGroup].Add(ConvertToMouseButtonInfo(m));
+                                break;
+                            case InputEventJoypadButton j:
+                                _defaultInputEvents[actionGroup].Add(ConvertToJoypadButtonInfo(j));
+                                break;
                         }
 
-                        if (inputEvent is InputEventMouseButton m)
-                        {
-                            _inputEvents[actionGroup].Add(
-                                new InputEventMouseButtonInfo {
-                                    InputEventInfoType = InputEventInfoType.MouseButton,
-                                    Alt = m.Alt,
-                                    Command = m.Command,
-                                    Control = m.Control,
-                                    Device = m.Device,
-                                    Meta = m.Meta,
-                                    Pressed = m.Pressed,
-                                    Shift = m.Shift,
-                                    ButtonIndex = m.ButtonIndex,
-                                    ButtonMask = m.ButtonMask,
-                                    DoubleClick = m.Doubleclick,
-                                    Factor = m.Factor,
-                                    GlobalPosition = m.GlobalPosition,
-                                    Position = m.Position
-                                }
-                            );
-                        }
-
-                        if (inputEvent is InputEventJoypadButton j)
-                        {
-                            _inputEvents[actionGroup].Add(
-                                new InputEventJoypadButtonInfo {
-                                    InputEventInfoType = InputEventInfoType.JoypadButton,
-                                    Device = j.Device,
-                                    Pressed = j.Pressed,
-                                    ButtonIndex = j.ButtonIndex,
-                                    Pressure = j.Pressure
-                                }
-                            );
-                        }
-
-                        uiHotkey.AddBtn(inputEvent.AsText());
+                        uiHotkey.AddBtn(inputEvent.Display());
                     }
                 }
             }
         }
+
+        public string GetCategory(string actionGroup)
+        {
+            var categories = new string[] {"ui", "player", "camera" };
+            foreach (var category in categories)
+                if (actionGroup.ToLower() == category.ToLower())
+                    return category.ToLower();
+
+            return categories[0];
+        }
+
+        public InputEventKeyInfo ConvertToKeyInfo(InputEventKey e) =>
+            new InputEventKeyInfo
+            {
+                InputEventInfoType = InputEventInfoType.Key,
+                Alt = e.Alt,
+                Command = e.Command,
+                Control = e.Control,
+                Device = e.Device,
+                Echo = e.Echo,
+                Meta = e.Meta,
+                PhysicalScancode = e.PhysicalScancode,
+                Pressed = e.Pressed,
+                Scancode = e.Scancode,
+                Shift = e.Shift,
+                Unicode = e.Unicode
+            };
+
+        public InputEventMouseButtonInfo ConvertToMouseButtonInfo(InputEventMouseButton e) =>
+            new InputEventMouseButtonInfo
+            {
+                InputEventInfoType = InputEventInfoType.MouseButton,
+                Alt = e.Alt,
+                Command = e.Command,
+                Control = e.Control,
+                Device = e.Device,
+                Meta = e.Meta,
+                Pressed = e.Pressed,
+                Shift = e.Shift,
+                ButtonIndex = e.ButtonIndex,
+                ButtonMask = e.ButtonMask,
+                DoubleClick = e.Doubleclick,
+                Factor = e.Factor,
+                GlobalPosition = e.GlobalPosition,
+                Position = e.Position
+            };
+
+        public InputEventJoypadButtonInfo ConvertToJoypadButtonInfo(InputEventJoypadButton e) =>
+            new InputEventJoypadButtonInfo
+            {
+                InputEventInfoType = InputEventInfoType.JoypadButton,
+                Device = e.Device,
+                Pressed = e.Pressed,
+                ButtonIndex = e.ButtonIndex,
+                Pressure = e.Pressure
+            };
     }
 
-    public class InputEventMouseButtonInfo : InputEventMouseInfo 
+    public class InputEventMouseButtonInfo : InputEventMouseInfo
     {
         public int ButtonIndex { get; set; }
         public bool DoubleClick { get; set; }
@@ -112,14 +133,14 @@ namespace GodotModules
         public bool Pressed { get; set; }
     }
 
-    public class InputEventMouseInfo : InputEventWithModifiersInfo 
+    public class InputEventMouseInfo : InputEventWithModifiersInfo
     {
         public int ButtonMask { get; set; }
         public Vector2 GlobalPosition { get; set; }
         public Vector2 Position { get; set; }
     }
 
-    public class InputEventKeyInfo : InputEventWithModifiersInfo 
+    public class InputEventKeyInfo : InputEventWithModifiersInfo
     {
         public bool Echo { get; set; }
         public uint PhysicalScancode { get; set; }
@@ -137,20 +158,26 @@ namespace GodotModules
         public bool Shift { get; set; }
     }
 
-    public class InputEventJoypadButtonInfo : InputEventInfo 
+    public class InputEventJoypadButtonInfo : InputEventInfo
     {
         public int ButtonIndex { get; set; }
         public bool Pressed { get; set; }
         public float Pressure { get; set; }
     }
 
-    public class InputEventInfo 
+    public class InputEventInfo
     {
         public InputEventInfoType InputEventInfoType { get; set; }
         public int Device { get; set; }
     }
 
-    public enum InputEventInfoType 
+    public class HotkeyInfo 
+    {
+        public string Category { get; set; }
+        public List<InputEventInfo> InputEventInfo { get; set; }
+    }
+
+    public enum InputEventInfoType
     {
         Key,
         MouseButton,
