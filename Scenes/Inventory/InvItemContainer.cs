4b3d60ba3f8aaf29932f282dd3e480ed3d1d977d
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-06-14 22:50:40 -0500

    InvItemContainers hold item data

--- Scenes/Inventory/InvItemContainer.cs
+++ Scenes/Inventory/InvItemContainer.cs
@@ -2,23 +2,25 @@ namespace GodotModules
 {
     public class InvItemContainer : PanelContainer
     {
+        public Item Item { get; private set; }
+        public Pos Pos { get; set; }
+        
         private Sprite _itemSprite;
         private bool _hasItem;
 
-        public void AddItem(string name)
+        public void SetItem(string name)
         {
+            Item = new Item(name);
             InitSprite(name);
         }
 
         private void InitSprite(string name)
         {
-            var texture = Items.Sprites[name];
-
             var sprite = new Sprite();
 
             var itemSize = RectSize - new Vector2(10, 10);
 
-            sprite.Texture = texture;
+            sprite.Texture = Items.Sprites[name];
             sprite.Position = RectSize / 2; // inv size
             sprite.Scale = itemSize / sprite.Texture.GetSize(); // item size
 
@@ -35,18 +37,44 @@ namespace GodotModules
             // left click
             if (!_hasItem)
             {
-                InitSprite("Item");
+                InitSprite("Item2");
                 return;
             }
 
             if (_hasItem) 
             {
+                GD.Print(Pos);
                 // change parent of item sprite to cursor item
                 _itemSprite.GetParent().RemoveChild(_itemSprite);
                 SceneInventory.CursorItem.AddChild(_itemSprite);
 
                 _hasItem = false;
             }
+        }
+    }
+
+    public class Item 
+    {
+        public string Name { get; set; }
+        public int Count { get; set; }
+
+        //private Sprite _sprite;
+
+        public Item(string name, int count = 1)
+        {
+            Name = name;
+            Count = count;
+        }
+
+        /*public void SetParent(Node parent)
+        {
+            _sprite.GetParent()?.RemoveChild(_sprite);
+            parent.AddChild(_sprite);
+        }*/
+
+        public Item Clone(Item item)
+        {
+            return new Item(Name, Count);
         }
     }
 }
